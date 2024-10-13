c9f0c55bb7b8b490bbdbd014ff18bffead16f862
kaung-minkhant <kaungminkhant.official@gmail.com>
2024-10-13 21:16:19 +0700

    something

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
-  description = "A very basic flake";
+  description = "Flake templates collections";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self }: {
+    templates = {
+      basic = {
+        path = ./basic;
+        description = "A very basic flake";
+      };
+    };
   };
 }
